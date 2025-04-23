803794d557049873b891ba2069294f5cc5103437
JORGE <jorgealmacen25@gmail.com>
2025-04-23 12:19:54 +0100

    navbar finished

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent {
+  isDarkMode = false;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    const storedDarkMode = localStorage.getItem('navbarDarkMode');
+    if (storedDarkMode === 'true') {
+      this.isDarkMode = true;
+    }
+  }
+
+  toggleDarkMode(): void {
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('navbarDarkMode', String(this.isDarkMode));
+  }
 }
