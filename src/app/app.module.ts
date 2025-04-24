1772d483dd56e55c79e03af27e12c084dd10b4d9
JORGE <jorgealmacen25@gmail.com>
2025-04-24 14:31:08 +0100

    work in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.model';
+import { NoteComponent } from './note/note.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
+    NoteComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
