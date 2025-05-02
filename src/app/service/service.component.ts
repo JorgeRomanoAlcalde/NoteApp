0c5ecce5c5779bda2050fbf595920c20cf4bd012
PalakMersus <98815347+palakMersus@users.noreply.github.com>
2025-05-02 15:06:16 +0100

    [Fix] Backend connection

--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -1,35 +1,21 @@
 import { HttpHeaders } from '@angular/common/http';
-import { Component, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient} from '@angular/common/http';
-
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class ServiceComponent {
 
   private headers = new HttpHeaders({
     'Content-Type': 'application/json'
   });
-
-  
   getHeader = { headers: this.headers, withCredentials: true };
 
-  constructor(private http: HttpClient){
-    this.testRequest();
+  constructor(private http: HttpClient) {}
+
+  public testRequest(): Observable<any> {
+    return this.http.get("http://localhost:3000/blogs", this.getHeader);
   }
-
-
-  public testRequest(): Observable<any>{
-    return this.http.get("http://localhost:1000/blogs",{
-      headers: this.getHeader.headers,
-      withCredentials: true,
-      responseType: 'text'
-    });
-  }
-
-  
 }
-
